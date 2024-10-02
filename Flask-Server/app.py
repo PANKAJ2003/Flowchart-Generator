e9427052cee04e85fabf6eb3d8c08f7c98633e9d
PANKAJ2003 <pankajmishra84454@gmail.com>
2024-10-02 11:16:09 +0530

    updated server

--- Flask-Server/app.py
+++ Flask-Server/app.py
@@ -107,4 +107,5 @@ def generate_flowchart():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
+
